89adf3844aa9886798e780d2ca8ac55b627056cd
jlt <582445069@qq.com>
2020-02-28 20:18:58 +0800

    add curd2.28

---
+++ jcart-administration-back/src/main/java/io/jlt/jcartadministrationback/controller/OrderHistoryController.java
@@ -0,0 +1,23 @@
+package io.jlt.jcartadministrationback.controller;
+
+import io.jlt.jcartadministrationback.dto.in.OrderHistoryCreateInDTO;
+import io.jlt.jcartadministrationback.dto.out.OrderHistoryListOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orderhistory")
+public class OrderHistoryController {
+
+    @GetMapping("/getListByOrderId")
+    public List<OrderHistoryListOutDTO> getListByOrderId(@RequestParam Long orderId){
+        return null;
+    }
+
+    @PostMapping("/create")
+    public Integer create(@RequestBody OrderHistoryCreateInDTO orderHistoryCreateInDTO){
+        return null;
+    }
+
+}
---
+++ jcart-administration-back/src/main/java/io/jlt/jcartadministrationback/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package io.jlt.jcartadministrationback.dao;
+
+import io.jlt.jcartadministrationback.po.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Long orderId);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Long orderId);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
